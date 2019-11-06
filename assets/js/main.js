63d7d01359e0ad6c0d654cbb024607bcaa6b5dce
Panyafuang <panyafuang@gmail.com>
2019-11-06 21:27:35 +0700

    edit vdo player

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,32 @@
-let myNav = document.querySelector('.navExpand');
+//
+// ─── NAVBAR EFFACT ──────────────────────────────────────────────────────────────
+//
 
+let myNav = document.querySelector('.navExpand');
 document.body.addEventListener('scroll', function(){
     if(document.body.scrollTop > 200 ){
+        // Add class .navShut
         myNav.classList.add('navShut');
     }else{
+        // Remove class .navShut
         myNav.classList.remove('navShut');
     }
 })
+
+
+//
+// ─── VIDEO PLAY ─────────────────────────────────────────────────────────────────
+//
+
+$(function () {
+    // Auto play modal video
+    $(".video").click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-video"),
+            videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
+});
