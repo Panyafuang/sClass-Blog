80af498e520c25e78516a6f2b9c746fc68993315
Panyafuang <panyafuang@gmail.com>
2019-11-15 19:55:15 +0700

    add blog-detail.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,30 @@ $(function () {
         });
     });
 });
+
+//
+// ─── OWL CAROUSEL ───────────────────────────────────────────────────────────────
+//
+
+    $(document).ready(function () {
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false
+                },
+                600: {
+                    items: 2,
+                    nav: false
+                },
+                1000: {
+                    items: 3,
+                    nav: false,
+                    loop: false
+                }
+            }
+        });
+    });
+
+
