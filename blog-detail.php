30883b542cf5e94982b561af621a23bc54dea701
Panyafuang <panyafuang@gmail.com>
2019-11-24 14:27:35 +0700

    update blog-detail

--- blog-detail.php
+++ blog-detail.php
@@ -1,41 +1,59 @@
+<?php
+    require_once('php/connect.php');
+
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+
+        $sql = "SELECT * FROM articles WHERE id = '".$id."' ";
+        $result = $conn->query($sql) or die ($conn->error);
+
+        if($result->num_rows > 0){
+            $row = $result->fetch_assoc();
+        }else{
+            header('Location: blog.php');
+        }
+    }
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <!--number-based width or "device-width"-->
-    <meta name="viewport" content="width=320, initial-scale=1, maximum-scale=1, user-scalable=0" />
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta property="viewport" content="width=320, initial-scale=1, maximum-scale=1, user-scalable=0" />
+    <!-- <meta property="viewport" content="width=device-width, initial-scale=1.0"> -->
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TP | เครื่องสำอางค์</title>
+    <title><?php echo $row['subject'];?></title>
 
     <!-- COMMON TAGS -->
     <!-- Search Engine -->
-    <meta name="description"
-        content="ช้อปออนไลน์ สุดยอดสินค้าขายดี แบรนด์ดังเพียบ ของแท้ 100% เช็คโปรโมชั่น เมคอัพ สุดคุ้ม อัพเดทเทรนใหม่ก่อนใคร เฉพาะที่นี่ที่เดียว ออนไลน์">
-    <meta name="keywords" content="เครื่องสำอางค์, เมคอัพ">
-    <meta name="title" content="ขายเครื่องสำอางค์ รีวิวเครื่องสำอางค์">
-    <meta name="robots" content="index, follow">
+    <meta property="description"
+        content="<?php echo $row['sub_title'];?>">
+    <meta property="keywords" content="เครื่องสำอางค์, เมคอัพ">
+    <meta property="title" content="ขายเครื่องสำอางค์ รีวิวเครื่องสำอางค์">
+    <meta property="robots" content="index, follow">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="language" content="English">
-    <meta name="author" content="Panyafuang">
-    <meta name="image"
+    <meta property="language" content="English">
+    <meta property="author" content="Panyafuang">
+    <meta property="image"
         content="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80">
     <!-- Schema.org for Google -->
-    <meta itemprop="name" content="TP | เครื่องสำอางค์">
-    <meta itemprop="description"
-        content="ช้อปออนไลน์ สุดยอดสินค้าขายดี แบรนด์ดังเพียบ ของแท้ 100% เช็คโปรโมชั่น เมคอัพ สุดคุ้ม อัพเดทเทรนใหม่ก่อนใคร เฉพาะที่นี่ที่เดียว ออนไลน์">
-    <meta itemprop="image"
+    <meta property="name" content="TP | เครื่องสำอางค์">
+    <meta property="description"
+        content="<?php echo $row['sub_title'];?>">
+    <meta property="image"
         content="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80">
     <!-- Open Graph general (Facebook, Pinterest & Google+) -->
-    <meta name="og:title" content="TP | เครื่องสำอางค์">
-    <meta name="og:description"
-        content="ช้อปออนไลน์ สุดยอดสินค้าขายดี แบรนด์ดังเพียบ ของแท้ 100% เช็คโปรโมชั่น เมคอัพ สุดคุ้ม อัพเดทเทรนใหม่ก่อนใคร เฉพาะที่นี่ที่เดียว ออนไลน์">
-    <meta name="og:image"
+    <meta property="og:title" content="TP | เครื่องสำอางค์">
+    <meta property="og:description"
+        content="<?php echo $row['sub_title'];?>">
+    <meta property="og:image"
         content="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80">
-    <meta name="og:url" content="https://blog-1706d.web.app/">
-    <meta name="og:site_name" content="https://blog-1706d.web.app/">
-    <meta name="og:type" content="website">
+    <meta property="og:url" content="https://blog-1706d.web.app/">
+    <meta property="og:site_name" content="https://blog-1706d.web.app/">
+    <meta property="og:type" content="website">
 
 
     <!-- Favicons -->
@@ -45,9 +63,9 @@
     <link rel="manifest" href="assets/images/favicons/site.webmanifest">
     <link rel="mask-icon" href="assets/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
     <link rel="shortcut icon" href="assets/images/favicons/favicon.ico">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-config" content="assets/images/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">
+    <meta property="msapplication-TileColor" content="#da532c">
+    <meta property="msapplication-config" content="assets/images/favicons/browserconfig.xml">
+    <meta property="theme-color" content="#ffffff">
 
 
     <!-- Bootstrap -->
@@ -65,11 +83,15 @@
     <?php include_once('includes/navbar.php'); ?>
 
     <!-- SECTION: PAGE TITLE -->
-    <header class="jarallax" data-jarallax='{ "speed": 0.5 }'
-        style="background-image: url('assets/images/blog-img/the-honest-company-oqmIM9bkAWQ-unsplash.jpg');">
+    <header class="jarallax" 
+        data-jarallax='{ "speed": 0.5 }'
+        style="
+            background-image: url('<?php echo $row['image'];?>');
+            background-size: cover;
+        ">
             <div class="blog-image">
-                <h1 class="display-4 brand-text font-weight-bold">Lips</h1>
-                <p class="lead">Fugiat iure blanditiis nihil consequuntur quibusdam recusandae sequi quaerat.</p>
+                <h1 class="display-4 brand-text font-weight-bold"><?php echo $row['subject'];?></h1>
+                <p class="lead"><?php echo $row['sub_title'];?></p>
             </div>
     </header>
 
@@ -77,34 +99,11 @@
     <section class="container blog-card">
         <div class="row">
             <div class="col-12">
-                <h1 class="text-center">Lips</h1>
-                <p><img src="https://images.unsplash.com/photo-1487412947147-5cebf100ffc2?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
-                        style="float:right" /> As
-                    the creators of a highly popular WYSIWYG rich text editor,&nbsp;<a
-                        href="https://cksource.com/">CKSource</a>&nbsp;is no stranger to web standards.&nbsp;<a
-                        href="https://ckeditor.com/demo">CKEditor</a>&nbsp;is responsible for producing a good part of
-                    the HTML content online so it must always generate semantic markup. Web standards are what keep the
-                    Internet from chaos and accessibility plays an important role in defining how the Web is shaped.
-                    CKSource is proud of being fully committed to the cause and thus&nbsp;CKEditor complies with most
-                    important industry standards, recommendations and checklists, including</p>
-
-                <p><strong>Accessibility Checker Is Here!</strong></p>
-
-                <p>Accessibility Checker is an innovative solution that lets you&nbsp;<strong>inspect the accessibility
-                        level</strong>&nbsp;of content created in CKEditor and&nbsp;<strong>immediately
-                        solve</strong>&nbsp;any accessibility issues that are found.</p>
-
-                <p>It is built upon three key elements:</p>
-
-                <p>1) <strong>Intuitive user interface</strong> optimized for quick problem solving.<br />
-                    2) Flexibility allowing you to use the <strong>accessibility checking engine</strong> of your
-                    choice.<br />
-                    3) <strong>Quick Fix</strong> feature letting you fix common problems fully automatically.</p>
-
+                <?php echo $row['detail']; ?>
             </div>
             <div class="col-12 text-right">
                 <hr>
-                <p class="text-muted">7 December 2019</p>
+                <p class="text-muted"><?php echo date_format(new DateTime($row['updated_at']), "j F Y"); ?></p>
                 <div class="pw-server-widget" data-id="wid-db6fpslc"></div>
             </div>
             <div class="col-12">
@@ -196,7 +195,7 @@
             </div>
             <!-- Facebook Comment -->
             <div class="col-12">
-                <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator2" data-width="100%"
+                <div class="fb-comments" data-href="http://localhost/phpFun/projects/blog/blog-detail.php?id_owlboss=<?php echo $row['id'];?>" data-width="100%"
                 data-numposts="5">
                 </div>
                 <div id="fb-root"></div>
