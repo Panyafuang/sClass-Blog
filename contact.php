f466ba6c5ac8263648c7fcdeaec6efeae22fc9ca
Panyafuang <panyafuang@gmail.com>
2019-11-26 20:47:34 +0700

    line notification

--- contact.php
+++ contact.php
@@ -68,7 +68,7 @@
             <div class="row">
                 <div class="col-12 text-center page-title-inner">
                     <h1 class="display-2 brand-text-thai">ข้อมูลการติดต่อ</h1>
-                    <p class="lead">Panyafuang Shop</p>
+                    <p class="lead">SurinStudio</p>
                 </div>
             </div>
         </div>
@@ -113,29 +113,31 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">แบบฟอร์มติดต่อเรา</h5>
-                        <form action="contact.php" method="POST">
+                        <form action="php/contact.php" method="POST">
                             <div class="form-row">
                                 <div class="form-group col-md-4">
                                     <label for="name">ชื่อ</label>
-                                    <input type="text" name="name" id="name" class="form-control" placeholder="ชื่อของคุณ">
+                                    <input type="text" name="name" id="name" class="form-control" placeholder="ชื่อของคุณ" required>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="phone">เบอร์โทรศัพท์</label>
-                                    <input type="number" name="phone" id="phone" class="form-control" placeholder="เบอร์โทรศัพท์">
+                                    <input type="number" name="phone" id="phone" class="form-control" placeholder="เบอร์โทรศัพท์" required>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="email">อีเมล์</label>
-                                    <input type="email" name="email" id="email" class="form-control" placeholder="example@gmali.com">
+                                    <input type="email" name="email" id="email" class="form-control" placeholder="example@gmali.com" required>
                                 </div>
                             </div>
                                 <div class="form-group">
-                                    <label for="msg">ข้อความของคุณ</label>
-                                    <textarea name="msg" id="msg" rows="5" class="form-control" placeholder="เขียนข้อความของคุณที่นี่"></textarea>
+                                    <label for="message">ข้อความของคุณ</label>
+                                    <textarea name="message" id="message" rows="5" class="form-control" placeholder="เขียนข้อความของคุณที่นี่" required></textarea>
                                 </div>
                                 <div id="recaptcha-wrapper" class="text-center mb-2">
-                                    <div class="g-recaptcha d-inline-block" data-sitekey="6LdEHMMUAAAAAE3KkT851bgYp-MvfjojDaSJrH-W"></div>
+                                    <div class="g-recaptcha d-inline-block" 
+                                    data-sitekey="6LdEHMMUAAAAAE3KkT851bgYp-MvfjojDaSJrH-W"
+                                    data-callback="checkCaptcha"></div>
                                 </div>
-                                <button type="submit" class="btn my-btn d-block mx-auto submit">ส่งข้อความ</button>
+                                <button type="submit" name="btn-submit" id="btn-submit" class="btn my-btn d-block mx-auto submit" disabled>ส่งข้อความ</button>
                         </form>
                     </div>
                 </div>
@@ -188,26 +190,38 @@
             resizeCaptcha();
         });
 
-        function resizeCaptcha() {
-            if (captchaWrapper.width() >= captchaWidth) {
-                if (captchaResized) {
-                    captchaElements.css('transform', '').css('-webkit-transform', '').css('-ms-transform', '').css('-o-transform',
-                    '').css('transform-origin', '').css('-webkit-transform-origin', '').css('-ms-transform-origin',
-                    '').css('-o-transform-origin', '');
-                    captchaWrapper.height(captchaHeight);
-                    captchaResized = false;
+        //
+        // ─── RESPONESIVE RECAPTCHA ───────────────────────────────────────
+        //
+
+            function resizeCaptcha() {
+                if (captchaWrapper.width() >= captchaWidth) {
+                    if (captchaResized) {
+                        captchaElements.css('transform', '').css('-webkit-transform', '').css('-ms-transform', '').css('-o-transform',
+                        '').css('transform-origin', '').css('-webkit-transform-origin', '').css('-ms-transform-origin',
+                        '').css('-o-transform-origin', '');
+                        captchaWrapper.height(captchaHeight);
+                        captchaResized = false;
+                    }
+                } else {
+                    var scale = (1 - (captchaWidth - captchaWrapper.width()) * (0.05/15));
+                    captchaElements.css('transform', 'scale('+scale+')').css('-webkit-transform',
+                    'scale('+scale+')').css('-ms-transform', 'scale('+scale+')').css('-o-transform',
+                    'scale('+scale+')').css('transform-origin', '0 0').css('-webkit-transform-origin', '0 0')
+                    .css('-ms-transform-origin', '0 0').css('-o-transform-origin', '0 0');
+                    captchaWrapper.height(captchaHeight * scale);
+                    if (captchaResized == false) captchaResized = true;
                 }
-            } else {
-                var scale = (1 - (captchaWidth - captchaWrapper.width()) * (0.05/15));
-                captchaElements.css('transform', 'scale('+scale+')').css('-webkit-transform',
-                'scale('+scale+')').css('-ms-transform', 'scale('+scale+')').css('-o-transform',
-                'scale('+scale+')').css('transform-origin', '0 0').css('-webkit-transform-origin', '0 0')
-                .css('-ms-transform-origin', '0 0').css('-o-transform-origin', '0 0');
-                captchaWrapper.height(captchaHeight * scale);
-                if (captchaResized == false) captchaResized = true;
             }
-        }
-        // resizeCaptcha();
+        
+
+        //
+        // ─── CHECK SUBMIT ────────────────────────────────────────────────
+        //
+
+            function checkCaptcha(){
+                document.querySelector('#btn-submit').disabled = false;
+            }
 
     </script>
 </body>
---
+++ php/contact.php
@@ -0,0 +1,96 @@
+<?php
+    require_once('connect.php');
+
+    if(isset($_POST['btn-submit'])){
+        $secretKey = '6LdEHMMUAAAAAAtkgBv-jH601bCjIzXSn3ep2sAr';
+        $response = $_POST['g-recaptcha-response'];
+        // IP domain client
+        $remoteip = $_SERVER['REMOTE_ADDR'];
+
+        $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$response&remoteip=$remoteip";
+        // Read content from url & covert json to array
+        $res = json_decode(file_get_contents($url));
+
+        // Check validation
+        if($res->success){
+            // If pass
+            // Insert to DB
+            $name = mysqli_real_escape_string($conn, $_POST['name']);
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+            $msg = mysqli_real_escape_string($conn, $_POST['message']);
+            
+            $sql = "INSERT INTO `contact` (`name`, `phone`, `email`, `message`, `created_at`) VALUES ('$name', '$phone', '$email', '$msg', '".date("Y-m-d")."' )";
+            $result = $conn->query($sql) or die($conn->error);
+
+            // Check Query
+            if($result){
+                    sentToLine();
+            }else{
+
+            }
+
+        }else{
+            // If not pass
+            echo '<script>alert("Valification Failed!!")</script>';
+            header('Refresh:0; url=../contact.php');
+        }
+    }else{
+        header('Refresh:0; url=../index.php');
+    }
+
+    
+    function sentToLine(){
+        // Concat var
+        $text = "มีผู้ติดต่อมาจากเว็บไซต์ \n";
+        $text .= "ชื่อ: ".$_POST['name']."\n";
+        $text .= "เบอร์: ".$_POST['phone']."\n";
+        $text .= "อีเมล์: ".$_POST['email']."\n";
+        $text .= "ข้อความ: ".$_POST['message'];
+        // Create array message
+        $message = array(
+            'message' => $text
+        );
+
+        // Invoke notify_message
+        notify_message($message);
+
+    }
+    
+    function notify_message($message){
+        // Create const var
+        define('LINE_API', "https://notify-api.line.me/api/notify");
+        define('LINE_TOKEN', "Vy3JMRl3TYUy1g1RJ4W6pfGWRBGziURc1zRKEyu0bqi");
+        // Generate URL-encoded query string
+        $queryData = http_build_query($message, '', '&');
+        // AJAX
+        $headerOptions = array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
+                            ."Authorization: Bearer ".LINE_TOKEN."\r\n"
+                            ."Content-Length: ".strlen($queryData)."\r\n",
+                'content' => $queryData
+            )
+        );
+
+        // Creates a stream context
+        $context = stream_context_create($headerOptions);
+        // Read message back from line
+        $result = file_get_contents(LINE_API,FALSE,$context);
+
+        // Convert json to array
+        $arrResult = json_decode($result);
+
+        if($arrResult->status == 200){
+            echo '<script>alert("ส่งข้อมูลสำเร็จ")</script>';
+            header('Refresh:0; url=../index.php');
+        }else{
+            echo '<script>alert("เกิดข้อผิดพลาด กรุณาลองใหม่ในภายหลัง!!")</script>';
+            header('Refresh:0; url=../contact.php');
+        }
+    }
+
+
+
+?>
